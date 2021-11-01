262c548409fca7bf6e17133251ba9a04b19ec7d9
Grishka3002 <grishapinchuk2013@gmail.com>
2021-11-01 14:13:41 -0500

    -

--- Задача6.py
+++ Задача6.py
@@ -1,3 +1,11 @@
 # Написать функцию сортирующую
-a  = list(map(int, input().split()))
-def 
+a  = list(map(int, input().split(",")))
+def sortirovka(a):
+    for i in range(len(a)):
+        minm = i
+        for j in range(i + 1, len(a)):
+            if a[j] < a[minm]:
+                minm = j
+        a[minm], a[i] = a[i], a[minm] 
+    print(a)    
+sortirovka(a)
